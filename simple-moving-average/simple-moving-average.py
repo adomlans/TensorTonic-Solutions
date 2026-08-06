fed3c106465d1898f8b941199038ad0f29080ab9
adomlans <3393979033@qq.com>
2026-08-06 19:39:05 +0800

    Solve simple-moving-average

---
+++ simple-moving-average/simple-moving-average.py
@@ -0,0 +1,9 @@
+def simple_moving_average(values, window_size):
+    """
+    Compute the simple moving average of the given values.
+    """
+    result = []
+    for i in range(len(values) - window_size + 1):
+        window = values[i:i + window_size]
+        result.append(sum(window) / window_size)
+    return result
