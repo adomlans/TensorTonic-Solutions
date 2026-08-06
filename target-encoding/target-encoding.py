edcce461d1fb531a01b0f8c787e1e9005ca67681
adomlans <3393979033@qq.com>
2026-08-06 12:20:52 +0800

    Solve target-encoding

---
+++ target-encoding/target-encoding.py
@@ -0,0 +1,11 @@
+def target_encoding(categories, targets):
+    """
+    Replace each category with the mean target value for that category.
+    """
+    sums = {}
+    counts = {}
+    for cat, t in zip(categories, targets):
+        sums[cat] = sums.get(cat, 0.0) + t
+        counts[cat] = counts.get(cat, 0) + 1
+    means = {cat: sums[cat] / counts[cat] for cat in sums}
+    return [means[cat] for cat in categories]
