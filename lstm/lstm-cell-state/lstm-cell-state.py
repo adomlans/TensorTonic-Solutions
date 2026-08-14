d4f4fdeac9d4b2db099427e16fc9a4d1904daabc
adomlans <3393979033@qq.com>
2026-08-14 11:00:22 +0800

    Solve lstm-cell-state

---
+++ lstm/lstm-cell-state/lstm-cell-state.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def update_cell_state(C_prev: np.ndarray, f_t: np.ndarray,
+                      i_t: np.ndarray, c_tilde: np.ndarray) -> np.ndarray:
+    C_t = f_t * C_prev + i_t * c_tilde
+    return C_t
