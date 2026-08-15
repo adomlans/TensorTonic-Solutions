a1e0dcde78249c0888a7bf84adac9a5028fe359a
adomlans <3393979033@qq.com>
2026-08-15 16:59:26 +0800

    Solve alexnet-pooling

---
+++ alexnet/alexnet-pooling/alexnet-pooling.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def max_pool2d(x: np.ndarray, kernel_size: int = 3, stride: int = 2) -> np.ndarray:
+    """Apply 2D max pooling (shape simulation)."""
+    B, H, W, C = x.shape
+    H_out = (H - kernel_size) // stride + 1
+    W_out = (W - kernel_size) // stride + 1
+    return np.zeros((B, H_out, W_out, C))
