e6d4dca166b88c7ee6abd0a527e9c24712f7a487
adomlans <3393979033@qq.com>
2026-08-30 18:24:46 +0800

    Solve triton-max

---
+++ study-plans/triton-basics/triton-max/triton-max.py
@@ -0,0 +1,20 @@
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def max_kernel(x_ptr, out_ptr, n, BLOCK_SIZE: tl.constexpr):
+    offsets = tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n
+
+    x = tl.load(x_ptr + offsets, mask=mask, other=-float('inf'))
+    block_max = tl.max(x, axis=0)
+    tl.store(out_ptr, block_max)
+
+
+def solve(x: torch.Tensor, out: torch.Tensor) -> None:
+    n = x.numel()
+    BLOCK_SIZE = triton.next_power_of_2(n)
+    grid = (1,)
+    max_kernel[grid](x, out, n, BLOCK_SIZE=BLOCK_SIZE)
